91df1c7b32d229847c463b4ba305cc7dfcdf0591
Anton <akai-world@narod.ru>
2021-11-29 21:04:19 +0200

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,9 +7,9 @@ const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
       <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
+      <span className={styles.navigationItem}>Borstch</span>
     </Link>
-    <ul className={styles.navigation}>
+    {/* <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
         <Link to="/" activeClassName="active">
           Home
@@ -20,7 +20,7 @@ const Navigation = () => (
           Blog
         </Link>
       </li>
-    </ul>
+    </ul> */}
   </nav>
 )
 
