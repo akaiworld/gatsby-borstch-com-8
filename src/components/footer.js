c4b13e9268eb1ef9693c1ad1197e0f88df7e341d
Anton <akai-world@narod.ru>
2023-08-19 15:07:44 +0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Container from './container'
-
+import * as styles from './footer.module.css'
 
 const Footer = () => (
   <Container as="footer">
-
+    <div className={styles.container}>
+      Borstch © {new Date().getFullYear()}
+    </div>
   </Container>
 )
 
